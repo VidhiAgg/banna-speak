bf7cdc6dfd2f273d9a568906a12e4a07981473f6
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2022-10-20 00:07:44 +0530

    Input and processing is done

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+//outputDiv.innerText = "Hii this is task"
 function clickHandler(){
-    console.log("clicked");
-    alert(txtInput.value);
-    console.log(txtInput.value);
+    outputDiv.innerText = "This will be translated to bannana: " + txtInput.value;
+
 };
 
 
 btnTranslate.addEventListener("click", clickHandler);
+
+
+
+// var btnprimary = document.querySelector('.btn-primary');
+// var inputTag = document.querySelector('#input-tag')
+//('#input[name=translator]')
