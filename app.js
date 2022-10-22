123dff88f96df0b3ba93f54e1712c1de172f2ba1
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2022-10-23 00:50:30 +0530

    Added API integration

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-//outputDiv.innerText = "Hii this is task"
-function clickHandler(){
-    outputDiv.innerText = "This will be translated to bannana: " + txtInput.value;
+var servelUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// //outputDiv.innerText = "Hii this is task"
+
+function getTranslationUrl(text) {
+    return servelUrl + "?text=" + text;
+}
+function errorHandler(error) {
+    console.log("Error occured: ", error);
+    alert("Something wrong with the server. Please try after sometime.")
+}
+
+function clickHandler() {
+    // outputDiv.innerText = "This will be translated to bannana: " + txtInput.value;
+    var inputText = txtInput.value; //taking input
+    //calling server for response
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(translation => console.log(translation.contents.translated))
+        .catch(errorHandler);
+
 
 };
 
@@ -11,7 +28,8 @@ function clickHandler(){
 btnTranslate.addEventListener("click", clickHandler);
 
 
-
 // var btnprimary = document.querySelector('.btn-primary');
 // var inputTag = document.querySelector('#input-tag')
-//('#input[name=translator]')
+//('#input[name=translator]') 
+
+
