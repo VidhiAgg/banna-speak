febfd81981867253922048d6a6235d2759424af6
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2022-10-23 01:33:23 +0530

    Added API link from funTranslation

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
+
+// get the btn which will be click, input contaning text, 
+//output textBox for showimng translation
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-var servelUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-// //outputDiv.innerText = "Hii this is task"
+var servelUrl = "https://api.funtranslations.com/translate/ferb-latin.json"
 
+//convert url by appending key and value to the url
 function getTranslationUrl(text) {
     return servelUrl + "?text=" + text;
 }
+
+//defining error for error
 function errorHandler(error) {
     console.log("Error occured: ", error);
     alert("Something wrong with the server. Please try after sometime.")
 }
 
+//when click happens 
+
 function clickHandler() {
-    // outputDiv.innerText = "This will be translated to bannana: " + txtInput.value;
-    var inputText = txtInput.value; //taking input
+    //outputDiv.innerText = "This will be translated to bannana: " + txtInput.value;
+    var inputText = txtInput.value; //reading the input
     //calling server for response
     fetch(getTranslationUrl(inputText))
         .then(response => response.json())
-        .then(translation => console.log(translation.contents.translated))
+        .then(json => {
+            var translatedText = json.contents.translation;
+            console.log(translatedText);
+            outputDiv.innerText = translatedText;
+        })
         .catch(errorHandler);
 
 
